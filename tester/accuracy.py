1c344bf5ed6bf08be2d29cfe458f6cf9d2c9799b
cangtianhuang <1903374751@qq.com>
2026-07-27 13:53:38 +0800

    🐛 Fix some bug

--- tester/accuracy.py
+++ tester/accuracy.py
@@ -525,11 +525,11 @@ class APITestAccuracy(APITestBase):
                 elif (
                     paddle_item is None
                     or (
-                        isinstance(paddle_item, paddle.Tensor) and not paddle_item._is_initialized()
+                        isinstance(paddle_item, paddle.Tensor)
+                        and not paddle_item._is_initialized()
+                        and int(paddle_item.numel()) != 0
                     )
                 ) and torch_item is None:
-                    # paddle is None and torch is None
-                    # paddle is Tensor but uninitialized and torch is None
                     tensor_index += 1
                 elif not isinstance(paddle_item, paddle.Tensor) or not isinstance(
                     torch_item, torch.Tensor
@@ -648,10 +648,9 @@ class APITestAccuracy(APITestBase):
                         or (
                             isinstance(paddle_item, paddle.Tensor)
                             and not paddle_item._is_initialized()
+                            and int(paddle_item.numel()) != 0
                         )
-                    ) and torch_item is None:
-                        # paddle is None and torch is None
-                        # paddle is Tensor but uninitialized and torch is None
+                    ) and (torch_item is None):
                         pass
                     elif not isinstance(paddle_item, paddle.Tensor) or not isinstance(
                         torch_item, torch.Tensor
