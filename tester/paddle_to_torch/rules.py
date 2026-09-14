86c3f7e8520af1f4be3df27cb98a6e2f28f29d5d
omoYang <108857999+omoYang@users.noreply.github.com>
2026-09-14 20:19:47 +0800

    1-D index下的paddle.gather映射由narrow+squeeze+stack改成torch.index_select (#726)

--- tester/paddle_to_torch/rules.py
+++ tester/paddle_to_torch/rules.py
@@ -3453,14 +3453,8 @@ elif index.dim() > 1:
             index = index.unsqueeze(0)
     result = torch.gather(x, axis, index)
 else:
-    ans = []
-    for i in index:
-        temp = torch.narrow(x, axis, i.reshape([]), 1)
-        ans.append(torch.squeeze(temp, axis))
-    if len(ans) == 0:
-        result = torch.zeros([x.shape[0],0])
-    else:
-        result = torch.stack(ans,axis)
+    # 1-D index: generator guarantees non-negative in-bounds index, so index_select matches paddle.gather
+    result = torch.index_select(x, axis, index)
 """
         return self.build_result(
             paddle_api,
